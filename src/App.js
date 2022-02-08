8ce8aad877a3991a8048c802be11c401ad03599e
FrancBM <franco-kemper-monroe@hotmail.com>
2022-02-07 23:16:34 -0500

    Tercera Entrega: Carrito

--- src/App.js
+++ src/App.js
@@ -1,21 +1,73 @@
-import Panda from './components/Panda';
-import Gorilla from './components/Gorilla';
-import Rhino from './components/Rhino';
-import SeaTurtle from './components/SeaTurtle';
-import Navigation from './components/navbar/Navigation';
-import { Routes, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Routes>
-        <Route path='/panda' component={Panda} />
-        <Route path='/gorilla' component={Gorilla} />
-        <Route path='/rhino' component={Rhino} />
-        <Route path='/seaTurtle' component={SeaTurtle} />
-      </Routes>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counters[index] };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counters[index] };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleRestart = () => {
+    window.location.reload();
+  };
+
+  render() {
+    return (
+      <div className="main__wrap">
+        <main className="container">
+          <div className="card__box">
+            <NavBar
+              totalCounters={
+                this.state.counters.filter((c) => c.value > 0).length
+              }
+            />
+            <Counters
+              counters={this.state.counters}
+              onReset={this.handleReset}
+              onIncrement={this.handleIncrement}
+              onDecrement={this.handleDecrement}
+              onDelete={this.handleDelete}
+              onRestart={this.handleRestart}
+            />
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
