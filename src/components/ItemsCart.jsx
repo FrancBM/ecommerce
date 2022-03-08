2111e4e90045213b4cd74282bb0e64768a2a637e
FrancBM <franco-kemper-monroe@hotmail.com>
2022-03-07 22:30:49 -0500

    Google Firestore

--- src/components/ItemsCart.jsx
+++ src/components/ItemsCart.jsx
@@ -10,55 +10,56 @@ import TableRow from '@material-ui/core/TableRow';
 
 
 const ItemsCart = (props) => {
-    return(                                       
+    return (
         <TableRow key={props.objeto.item.id}>
-                <TableCell component="th" scope="row" align="center" >
-                    <img src={props.objeto.item.thumbnail} alt={`${props.objeto.item.title}`} style={{backgroundSize: 'contain', height:60}}/>                    
-                </TableCell>                                    
-                <TableCell align="left" >
-                    {props.objeto.item.title}                    
-                </TableCell>
-                <TableCell align="center">
-                    {props.objeto.item.price} $
-                </TableCell>
-                <TableCell align="center">
+            <TableCell component="th" scope="row" align="center" >
+                <img src={props.objeto.item.thumbnail} alt={`${props.objeto.item.title}`} style={{ backgroundSize: 'contain', height: 60 }} />
+            </TableCell>
+            <TableCell align="left" >
+                {props.objeto.item.title}
+            </TableCell>
+            <TableCell align="center">
+                {props.objeto.item.price} $
+            </TableCell>
+            <TableCell align="center">
+                <IconButton
+                    variant="contained"
+                    color="secondary"
+                    size="small"
+                    onClick={() => { props.remover(props.objeto.item.id) }}>
+                    <DeleteForeverIcon />
+                </IconButton>
+            </TableCell>
+            <TableCell align="center">
+                <Grid item xs={2} sm={2} style={{ display: "flex", alignItems: "center" }}>
                     <IconButton
-                        variant="contained"
-                        color="secondary"
-                        size="small"
-                        onClick={()=> {props.remover(props.objeto.item.id)}}>
-                            <DeleteForeverIcon />
-                    </IconButton>
-                </TableCell>
-                <TableCell align="center">
-                    <Grid item xs={2} sm={2} style={{display:"flex", alignItems:"center"}}>                
-                        <IconButton
                         size="medium"
                         variant="contained"
                         color="secondary"
-                        onClick={()=>{ props.substractOne(props.objeto.item.id) }}
-                        >
-                            <RemoveCircle />
-                        </IconButton> 
-                        <Typography variant="body1" component="p" style={{textAlign:"center"}}>
-                            {props.objeto.quantity}
-                        </Typography>
-                        <IconButton
-                        size="medium" 
+                        onClick={() => { props.substractOne(props.objeto.item.id) }}
+                    >
+                        <RemoveCircle />
+                    </IconButton>
+                    <Typography variant="body1" component="p" style={{ textAlign: "center" }}>
+                        {props.objeto.quantity}
+                    </Typography>
+                    <IconButton
+                        size="medium"
                         variant="contained"
                         color="primary"
-                        onClick={()=>{ props.addOne(props.objeto.item.id) }}
-                        >
-                            <AddCircle />
-                        </IconButton>
-                    </Grid>
-                </TableCell><TableCell align="center">
-                    {props.objeto.item.available_quantity - props.objeto.quantity}
-                </TableCell> 
-                <TableCell align="center">
-                    {props.objeto.quantity*props.objeto.item.price} $
-                </TableCell>                   
-            </TableRow> 
-)}
+                        onClick={() => { props.addOne(props.objeto.item.id) }}
+                    >
+                        <AddCircle />
+                    </IconButton>
+                </Grid>
+            </TableCell><TableCell align="center">
+                {props.objeto.item.available_quantity - props.objeto.quantity}
+            </TableCell>
+            <TableCell align="center">
+                {props.objeto.quantity * props.objeto.item.price} $
+            </TableCell>
+        </TableRow>
+    )
+}
 
 export default ItemsCart
