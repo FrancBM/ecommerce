abea8a7e93ca158efe48766a56ca1e5499a6f784
FrancBM <franco-kemper-monroe@hotmail.com>
2022-03-27 20:35:15 -0500

    Entrega final

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,10 @@
-import React, {useContext, useEffect} from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Grid, Typography } from "@material-ui/core";
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Button from "@material-ui/core/Button";
 import ItemsCart from './ItemsCart'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -13,40 +13,40 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-import {CartContext} from '../context/CartContext';
+import { CartContext } from '../context/CartContext';
 
 const StyledCell = {
     fontWeight: "bolder",
     color: "white"
 }
 const Cart = () => {
-    const {removeItem,clear,cart,total, getTotal, addOneItem,substractOneItem} = useContext(CartContext);
-    
+    const { removeItem, clear, cart, total, getTotal, addOneItem, substractOneItem } = useContext(CartContext);
+
     useEffect(() => {
         getTotal()
     }, [cart])
 
-    return(
-        <Box px={{xs:3 , sm:5}} py={{xs:10, sm:10}} style={{minHeight:"53vh"}}>
-            <Container maxWidth = "lg">
-                {cart.length === 0? (
+    return (
+        <Box px={{ xs: 3, sm: 5 }} py={{ xs: 10, sm: 10 }} style={{ minHeight: "53vh" }}>
+            <Container maxWidth="lg">
+                {cart.length === 0 ? (
                     <Grid container spacing={5}>
                         <Grid item xs={12} sm={12}>
-                            <Typography variant="h4" component="p" style={{textAlign:"center"}}>
-                                El Carrito está vacío, haz 
-                                <Link style={{paddingLeft: 10, paddingRight:10}} to={`/`}>
-                                click acá
-                                </Link> 
+                            <Typography variant="h4" component="p" style={{ textAlign: "center" }}>
+                                El Carrito está vacío, haz
+                                <Link style={{ paddingLeft: 10, paddingRight: 10 }} to={`/`}>
+                                    click acá
+                                </Link>
                                 para buscar tu producto.
                             </Typography>
                         </Grid>
                     </Grid>
-                ):(
+                ) : (
                     <Grid container spacing={5}>
                         <TableContainer component={Paper}>
                             <Table aria-label="customized table">
                                 <TableHead>
-                                    <TableRow style={{backgroundColor: "gray"}}>
+                                    <TableRow style={{ backgroundColor: "gray" }}>
                                         <TableCell style={StyledCell}>Productos</TableCell>
                                         <TableCell style={StyledCell} align="center">Descripción</TableCell>
                                         <TableCell style={StyledCell} align="center">Precio Unit.</TableCell>
@@ -56,41 +56,41 @@ const Cart = () => {
                                         <TableCell style={StyledCell} align="center">Subtotal</TableCell>
                                     </TableRow>
                                 </TableHead>
-                                <TableBody> 
-                                    {cart.map((element)=>(
-                                        <ItemsCart key={element.item.id} objeto={element} remover={removeItem} addOne={addOneItem} substractOne={substractOneItem}/>
+                                <TableBody>
+                                    {cart.map((element) => (
+                                        <ItemsCart key={element.item.id} objeto={element} remover={removeItem} addOne={addOneItem} substractOne={substractOneItem} />
                                     ))}
-                                        <TableRow key={"total"}>
-                                            <TableCell component="th" scope="row" align="center" ></TableCell>                                    
-                                            <TableCell align="left" ></TableCell>
-                                            <TableCell align="left" ></TableCell>
-                                            <TableCell align="left" ></TableCell>
-                                            <TableCell align="left" >Total a Pagar:</TableCell>
-                                            <TableCell align="left" style={{fontWeight: "bold", fontSize: 20}}>{total} $</TableCell>
-                                        </TableRow>                    
+                                    <TableRow key={"total"}>
+                                        <TableCell component="th" scope="row" align="center" ></TableCell>
+                                        <TableCell align="left" ></TableCell>
+                                        <TableCell align="left" ></TableCell>
+                                        <TableCell align="left" ></TableCell>
+                                        <TableCell align="left" >Total a Pagar:</TableCell>
+                                        <TableCell align="left" style={{ fontWeight: "bold", fontSize: 20 }}>{total} $</TableCell>
+                                    </TableRow>
                                 </TableBody>
                             </Table>
-                        </TableContainer> 
+                        </TableContainer>
                     </Grid>
                 )}
-                <Grid container spacing={5} style={{display:"flex", justifyContent:"flex-end", padding:50}}>
-                    <Button                        
-                    variant="contained"
-                    color="secondary"
-                    size="small"
-                    onClick={()=> {clear()}} > 
-                    Limpiar el Carrito 
+                <Grid container spacing={5} style={{ display: "flex", justifyContent: "flex-end", padding: 50 }}>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        size="small"
+                        onClick={() => { clear() }} >
+                        Limpiar el Carrito
                     </Button>
                     <Button
-                    disabled={cart.length === 0}                   
-                    variant="contained"
-                    color="primary"
-                    size="small"
-                    onClick={()=> {}}
-                    style={{marginLeft:15}} 
-                    > 
-                        <Link to={`/formBuy`} style={{color:'white'}}>
-                            Continuar Compra 
+                        disabled={cart.length === 0}
+                        variant="contained"
+                        color="primary"
+                        size="small"
+                        onClick={() => { }}
+                        style={{ marginLeft: 15 }}
+                    >
+                        <Link to={`/formBuy`} style={{ color: 'white' }}>
+                            Continuar Compra
                         </Link>
                     </Button>
                 </Grid>
